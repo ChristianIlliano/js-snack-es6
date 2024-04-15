81b996ff027240f63411e5b9cb198b434aa37a7b
ChristianIlliano <grafica.illiano@gmail.com>
2024-04-15 15:31:08 +0200

    feat: scaffolding

---
+++ Snack-1/js/script.js
@@ -0,0 +1,17 @@
+const bike = [
+{
+    name: "Cervelo",
+    weight: "15",
+},
+{
+    name: "Pinarello",
+    weight: "18",
+},
+{
+    name: "Colnago",
+    weight: "13",
+},
+]
+
+const {name, weight} = bike;
+console.log(name, weight);
