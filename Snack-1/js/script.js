ae9ab482d703bbf96e002298018dff88fec4ee9c
ChristianIlliano <grafica.illiano@gmail.com>
2024-04-15 17:13:25 +0200

    feat: snack1

--- Snack-1/js/script.js
+++ Snack-1/js/script.js
@@ -1,17 +1,44 @@
 const bike = [
 {
-    name: "Cervelo",
-    weight: "15",
+    name: "bike1",
+    weight: 15,
 },
 {
-    name: "Pinarello",
-    weight: "18",
+    name: "bike2",
+    weight: 18,
 },
 {
-    name: "Colnago",
-    weight: "13",
+    name: "bike3",
+    weight: 18,
+},
+
+{
+    name: "bike4",
+    weight: 13,
+},
+
+{
+    name: "bike5",
+    weight: 10,
 },
 ]
 
-const {name, weight} = bike;
-console.log(name, weight);
+
+const minBike = document.getElementById("minBike");
+let weightMin = bike[0].weight;
+let indexWeightMin = 0;
+
+    bike.forEach((element, index) => {
+        const {weight} = element;
+        console.log(weight, weightMin);
+
+        if (weight < weightMin) {
+            weightMin = weight;
+            indexWeightMin = index;
+            console.log(indexWeightMin);
+        }
+
+    })
+
+    minBike.innerHTML = `${bike[indexWeightMin].name}`
+    console.log(bike[indexWeightMin].name);
