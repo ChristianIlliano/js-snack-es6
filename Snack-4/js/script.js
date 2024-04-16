c302da7692a49bb3b0f3006b63c338630a66e8f5
ChristianIlliano <grafica.illiano@gmail.com>
2024-04-16 12:40:56 +0200

    feat: fix snack 4

--- Snack-4/js/script.js
+++ Snack-4/js/script.js
@@ -1,29 +1,17 @@
-let persone = [
+const persone = [
     { nome: "Mario", cognome: "Rossi", età: 30 },
     { nome: "Anna", cognome: "Bianchi", età: 17 },
     { nome: "Luca", cognome: "Verdi", età: 40 }
 ]
 
-// persone.forEach((curPerson) => {
-//     curPerson = {nome, cognome}
-
-//     if (persone.età > 18) {
-//         return result = "idoneo"
-//     } else {
-//         result = "non idoneo"
-//     }
-
-// });
-
 const arrPersone = persone.map((curPerson) => {
 
-    if (persone.età >= 18) {
-        return `${persone.nome} ${persone.cognome} può guidare`; 
+    if (curPerson.età >= 18) {
+        return `${curPerson.nome} ${curPerson.cognome} può guidare`; 
     } else {
-        return `${persone.nome} ${persone.cognome} non può guidare`; 
+        return `${curPerson.nome} ${curPerson.cognome} non può guidare`; 
     }
 });
 
 console.log(arrPersone);
 
-// Creazione dell'array di persone
